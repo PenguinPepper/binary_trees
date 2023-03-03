3f13682364355234c4af04d9076cb9d5845a606b
Elizabeth <testlolabtest@gmail.com>
2023-03-03 14:30:39 -0800

    Add file for task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ *
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if true and 0 if false
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+		return (1);
+}
