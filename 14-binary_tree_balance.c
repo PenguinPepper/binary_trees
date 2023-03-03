41c32fd8f9e60a0e715f602827384d249242bb7c
Elizabeth <testlolabtest@gmail.com>
2023-03-03 13:24:57 -0800

    Check if tree is full

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_balance - function that measures the balance factor of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l, r;
+
+	if (tree == NULL)
+		return (0);
+}
