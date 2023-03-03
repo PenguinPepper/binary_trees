7ed40d2775c551e477cb472db270ab9bb72d9fb2
Elizabeth <testlolabtest@gmail.com>
2023-03-03 12:12:44 -0800

    Appy the logic of 2h +1 to get the size of the tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,22 +10,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t num_nodes = 1;
-	binary_tree_t *not_leaf, *not_rileaf;
-
 	if (tree == NULL)
 		return (0);
-	not_leaf = tree->left;
-	while (not_leaf)
-	{
-		num_nodes++;
-		not_leaf = not_leaf->left;
-	}
-	not_rileaf = tree->right;
-	while (not_rileaf)
-	{
-		num_nodes++;
-		not_rileaf = not_rileaf->right;
-	}
-	return (num_nodes);
+	return (binary_tree_size(tree->left) + 1 +
+			binary_tree_size(tree->right));
 }
